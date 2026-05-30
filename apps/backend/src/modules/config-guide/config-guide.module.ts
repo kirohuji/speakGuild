24f15df60adc33d95847ebb429f14161647a8b41
lourd <z1309014381@gmail.com>
2026-05-30 20:56:16 +0800

    code: 删除不需要的功能

--- apps/backend/src/modules/config-guide/config-guide.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigGuideController } from './config-guide.controller';
-import { ConfigGuideService } from './config-guide.service';
-
-@Module({
-  controllers: [ConfigGuideController],
-  providers: [ConfigGuideService],
-})
-export class ConfigGuideModule {}
